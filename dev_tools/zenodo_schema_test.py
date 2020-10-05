f0066413e886a868def7ad65649811bd170f7f60
Bálint Pató <balintp@google.com>
2020-10-05 17:02:54 -0400

    now?

--- dev_tools/zenodo_schema_test.py
+++ dev_tools/zenodo_schema_test.py
@@ -8,8 +8,8 @@ import pytest
 
 def test_zenodo_config():
     ignore_ssl = ssl.SSLContext()
-    schema_url = ('https://zenodo.org/schemas/records/record-v1.0.0.json')
-    with urlopen(schema_url, context=ignore_ssl) as f_schema:
+    # schema_url = ('https://zenodo.org/schemas/records/record-v1.0.0.json')
+    with open('dev_tools/zenodo_schema.json') as f_schema:
         schema = json.load(f_schema)
         with open(".zenodo.json", "rb") as f_config:
             config = json.load(f_config)
